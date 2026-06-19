9e3497e2927ba30e7e55f1f50983e90588123492
Humphrey Nyahoja <h.nyahoja@alustudent.com>
2026-06-19 22:29:53 +0200

    I ADDED #app/app/__pycache__/__init__.cpython-312.pyc,#app/app/__pycache__/settings.cpython-312.pyc #app/app/__pycache__/urls.cpython-312.pyc,#app/app/__pycache__/wsgi.cpython-312.pyc #app/asked_app/__pycache__/admin.cpython-312.pyc,#app/asked_app/__pycache__/apps.cpython-312.pyc #app/asked_app/__pycache__/models.cpython-312.pyc,#app/asked_app/migrations/__pycache__/__init__.cpython-312.pyc #app/asked_app/templates/asked_app/index.html,#app/db.sqlite3, UPDATED #app/app/settings.py,#app/app/urls.py #app/asked_app/__pycache__/views.cpython-312.pyc,#app/asked_app/views.py

--- app/asked_app/views.py
+++ app/asked_app/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+#from django.http import HttpResponse
+#from django.template import loader
+
+#def index(request):
+#    template = loader.get_template('asked_app/template/index.html')
+ #   return HttpResponse(template.render())
 
 def index(request):
-    return HttpResponse("Hello world!")
+    return render(request, 'asked_app/index.html')
